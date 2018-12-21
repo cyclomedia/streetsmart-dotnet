ca0a7b66c7fbfff3f7eb549ecd5102a28b8959e1
Harm Bruinsma <hbruinsma@cyclomedia.com>
2018-12-21 14:12:13 +0100

    Updated assembly info

--- StreetSmartAPI/StreetSmart.WPF.Example/Properties/AssemblyInfo.cs
+++ StreetSmartAPI/StreetSmart.WPF.Example/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.14.0")]
-[assembly: AssemblyFileVersion("18.14.0.0")]
+[assembly: AssemblyVersion("18.15.0")]
+[assembly: AssemblyFileVersion("18.15.0.0")]
