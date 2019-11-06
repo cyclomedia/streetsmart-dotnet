a49393540c94aa2a82867f13f0c6039113c04463
Harm73 <harmbruinsma@home.nl>
2019-11-06 09:18:10 +0100

    STREET-2526: - Updated version nummber - the 2 event files

---
+++ StreetSmartAPI/StreetSmart.API/Common/API/Events/ViewerRemovedEvent.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Street Smart .NET integration
+ * Copyright (c) 2016 - 2019, CycloMedia, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+namespace StreetSmart.Common.API.Events
+{
+  internal class ViewerRemovedEvent: StreetSmartApiEvent
+  {
+    private string TempType => $"temp{Type}";
+
+    public ViewerRemovedEvent(StreetSmartAPI api, string type, string funcName)
+      : base(api, type, funcName, "viewer")
+    {
+    }
+
+    public override string ToString()
+    {
+      return $@"var {TempType};{JsApi}.on({JsApi}.{Events}.{Type},{FuncName}{Category}=function(e)
+             {{{TempType}=e.detail.viewer;if({TempType}!==undefined&&{TempType}!==undefined){{{JsThis}.{FuncName}({TempType}.getId(),{TempType}.getType());}}}});";
+    }
+  }
+}
