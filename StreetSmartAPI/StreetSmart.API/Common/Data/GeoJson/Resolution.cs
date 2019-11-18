938e175e35f7b789c7447586c660fcc176c8353a
Harm73 <harmbruinsma@home.nl>
2019-11-18 11:58:01 +0100

    STREET-2539

---
+++ StreetSmartAPI/StreetSmart.API/Common/Data/GeoJson/Resolution.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Street Smart .NET integration
+ * Copyright (c) 2016 - 2019, CycloMedia, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+using System.Collections.Generic;
+
+using StreetSmart.Common.Interfaces.GeoJson;
+
+namespace StreetSmart.Common.Data.GeoJson
+{
+  // ReSharper disable once InconsistentNaming
+  internal class Resolution : DataConvert, IResolution
+  {
+    public Resolution(Dictionary<string, object> info)
+    {
+      Dictionary<string, object> resolution = GetDictValue(info, "resolutionOfImage");
+      Width = ToInt(resolution, "width");
+      Stdev = ToInt(resolution, "stdev");
+      Error = ToInt(resolution, "error");
+    }
+
+    public Resolution(IResolution resolution)
+    {
+      if (resolution != null)
+      {
+        Width = resolution.Width;
+        Stdev = resolution.Stdev;
+        Error = resolution.Error;
+      }
+    }
+
+    public int Width { get; }
+
+    public int Stdev { get; }
+
+    public int Error { get; }
+
+    public override string ToString()
+    {
+      return $"\"resolutionOfImage\":{{\"width\":\"{Width}\",\"stdev\":{Stdev},\"error\":{Error}}}";
+    }
+  }
+}
---
+++ StreetSmartAPI/StreetSmart.API/Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Street Smart .NET integration
+ * Copyright (c) 2016 - 2019, CycloMedia, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+// ReSharper disable once CheckNamespace
+
+using System.Drawing;
+using System.Globalization;
+
+namespace System
+{
+  internal static class DateTimeExtensions
+  {
+    public static string ToJsDateTime(this DateTime? value)
+    {
+      DateTimeFormatInfo ff = new DateTimeFormatInfo();
+      return value == null ? string.Empty : ((DateTime) value).ToString(ff.SortableDateTimePattern);
+    }
+  }
+}
