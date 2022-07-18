2210e1912e7a63b39e5f5d90c1c7208c5bd9ec4f
Harm Bruinsma <harmbruinsma@home.nl>
2022-07-18 10:12:23 +0200

    STREET-4723: Create package for the new .net api

--- StreetSmartAPI/StreetSmart.WPF.Example/Properties/AssemblyInfo.cs
+++ StreetSmartAPI/StreetSmart.WPF.Example/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.7.0")]
-[assembly: AssemblyFileVersion("22.7.0.0")]
+[assembly: AssemblyVersion("22.11.0")]
+[assembly: AssemblyFileVersion("22.11.0.0")]
