e1fc3c4585987b96b3cda59b9293029c9c089be8
Harm Bruinsma <harmbruinsma@home.nl>
2022-11-30 11:59:45 +0100

    STREET-5219: to new version 22.19

--- StreetSmartAPI/StreetSmart.WPF.Example/Properties/AssemblyInfo.cs
+++ StreetSmartAPI/StreetSmart.WPF.Example/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.18.0")]
-[assembly: AssemblyFileVersion("22.18.0.0")]
+[assembly: AssemblyVersion("22.19.0")]
+[assembly: AssemblyFileVersion("22.19.0.0")]
