0d45db35f549e5efe08a480fa0bc915277b248d6
Harm Bruinsma <harmbruinsma@home.nl>
2023-01-20 15:00:29 +0100

    STREET-5428: .NET API 23.1

--- StreetSmartAPI/StreetSmart.API/WPF/Properties/Resources.Designer.cs
+++ StreetSmartAPI/StreetSmart.API/WPF/Properties/Resources.Designer.cs
@@ -97,7 +97,7 @@ namespace StreetSmart.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://streetsmart.cyclomedia.com/api/v22.20/api-dotnet.html.
+        ///   Looks up a localized string similar to https://streetsmart.cyclomedia.com/api/v23.1/api-dotnet.html.
         /// </summary>
         internal static string StreetSmartLocation {
             get {
