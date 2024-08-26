896e9f5b7b609764b97c9dc53fffe23e5764597b
Aleksa Milivojevic <amilivojevic@cyclomedia.com>
2024-08-26 06:24:24 -0700

    Added new tests to check for various transparency levels and scenarios where only one color (red, green or blue) is chosen.

--- StreetSmartAPI/StreetSmart.WPF.Tests/SvgParameterCollectionTests.cs
+++ StreetSmartAPI/StreetSmart.WPF.Tests/SvgParameterCollectionTests.cs
@@ -11,7 +11,7 @@ public class SvgParameterCollectionTests
   public void GetFillObject_AllZeroColor_NullOpacity_ReturnsExpectedResult()
   {
     // Arrange
-    Color color = Color.FromArgb(255, 0, 0, 0);
+    Color color = Color.FromArgb(0, 0, 0, 0);
     double? opacity = null;
     string expectedColorValue = "#000000";
 
@@ -28,11 +28,12 @@ public class SvgParameterCollectionTests
     Assert.Equal(color.G, resultGreen);
     Assert.Equal(color.B, resultBlue);
   }
+
   [Fact]
-  public void GetFillObject_DifferentColor_NullOpacity_ReturnsExpectedResult()
+  public void GetFillObject_DifferentColorFullTransparency_NullOpacity_ReturnsExpectedResult()
   {
     // Arrange
-    Color color = Color.FromArgb(10, 128, 64, 32); 
+    Color color = Color.FromArgb(0, 128, 64, 32);
     double? opacity = null;
     string expectedColorValue = "#804020";
 
@@ -49,6 +50,95 @@ public class SvgParameterCollectionTests
     Assert.Equal(color.G, resultGreen);
     Assert.Equal(color.B, resultBlue);
   }
+  [Fact]
+  public void GetFillObject_DifferentColor_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 128, 64, 32);
+    double? opacity = null;
+    string expectedColorValue = "#804020";
+
+    // Act
+    var result = SvgParameterCollection<FillType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetFillObject_AllZeroColorExceptTransparency_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 0, 0, 0);
+    double? opacity = null;
+    string expectedColorValue = "#000000";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetFillObject_AllZeroColorExceptTransparencyWhichIsOn10_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(10, 0, 0, 0);
+    double? opacity = null;
+    string expectedColorValue = "#000000";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetFillObject_DifferentColorHighTransparency_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(10, 128, 64, 32);
+    double? opacity = null;
+    string expectedColorValue = "#804020";
+
+    // Act
+    var result = SvgParameterCollection<FillType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  
 
   [Fact]
   public void GetStrokeObject_AllZeroColor_NullWidthAndOpacity_ReturnsExpectedResult()
@@ -93,6 +183,230 @@ public class SvgParameterCollectionTests
     Assert.Equal(color.G, resultGreen);
     Assert.Equal(color.B, resultBlue);
   }
+
+  [Fact]
+  public void GetStrokeObject_AllZeroColorExceptTransparencyIs10_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(10, 0, 0, 0);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#000000";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+  [Fact]
+  public void GetStrokeObject_DifferentColorExceptTransparencyIs10_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(10, 128, 64, 32);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#804020";
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetStrokeObject_AllZeroColorExceptTransparencyIs255_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 0, 0, 0);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#000000";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+  [Fact]
+  public void GetStrokeObject_DifferentColorExceptTransparencyIs255_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 128, 64, 32);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#804020";
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetFillObject_OnlyRedColor_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 255, 0, 0);
+    double? opacity = null;
+    string expectedColorValue = "#FF0000";
+
+    // Act
+    var result = SvgParameterCollection<FillType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetFillObject_OnlyGreenColor_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 0, 255, 0);
+    double? opacity = null;
+    string expectedColorValue = "#00FF00";
+
+    // Act
+    var result = SvgParameterCollection<FillType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetFillObject_OnlyBlueColor_NullOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 0, 0, 255);
+    double? opacity = null;
+    string expectedColorValue = "#0000FF";
+
+    // Act
+    var result = SvgParameterCollection<FillType>.GetFillObject(color, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(FillType.Fill, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetStrokeObject_OnlyRedColor_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 255, 0, 0);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#FF0000";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetStrokeObject_OnlyGreenColor_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 0, 255, 0);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#00FF00";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
+  [Fact]
+  public void GetStrokeObject_OnlyBlueColor_NullWidthAndOpacity_ReturnsExpectedResult()
+  {
+    // Arrange
+    Color color = Color.FromArgb(255, 0, 0, 255);
+    double? width = null;
+    double? opacity = null;
+    string expectedColorValue = "#0000FF";
+
+    // Act
+    var result = SvgParameterCollection<StrokeType>.GetStrokeObject(color, width, opacity);
+    var (resultRed, resultGreen, resultBlue) = HexToRgb(result.SvgParameter[0].Value);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.Single(result.SvgParameter);
+    Assert.Equal(StrokeType.Stroke, result.SvgParameter[0].Name);
+    Assert.Equal(expectedColorValue, result.SvgParameter[0].Value);
+    Assert.Equal(color.R, resultRed);
+    Assert.Equal(color.G, resultGreen);
+    Assert.Equal(color.B, resultBlue);
+  }
+
 
   public static (int Red, int Green, int Blue) HexToRgb(string hex)
   {
